e48a709f6725fe9bcfd6857414f3e53b922ab320
nicolasValcarenghi <nicki.valca10@gmail.com>
2024-09-03 09:02:07 -0300

    aula/03/08

--- resources/views/animais/cadastrar.blade.php
+++ resources/views/animais/cadastrar.blade.php
@@ -16,12 +16,26 @@
 </div>  
 @endif
 
-<form method="post" action="{{ route('animais.gravar') }}">
+<form enctype="multipart/form-data" class="p-10 bg-white rounded shadow-xl" method="post" action="{{ route('animais.gravar') }}">
     @csrf
-    <input type="text" name="nome" placeholder="Nome" value="{{ old('nome') }}">
+    <div class="">
+        <label class="block text-sm text-gray-600" for="nome">Nome</label>
+        <input type="text" name="nome" placeholder="Nome" value="{{ old('nome') }}" class="w-full px-5 py-1 text-gray-700 bg-gray-200 rounded" id="nome" name="nome" type="text" 
+        required="" placeholder="Seu Nome" aria-label="Nome">
+    </div>
     <br>
-    <input type="number" name="idade" placeholder="Idade" value="{{ old('idade') }}">
+    <div class="">
+        <label class="block text-sm text-gray-600" for="imagem">Imagem</label>
+        <input type="file"  name="imagem" placeholder="imagem" value="{{ old('imagem') }}" class="w-full px-5 py-1 text-gray-700 bg-gray-200 rounded" id="imagem" name="imagem"
+        required="" placeholder="Sua imagem" aria-label="imagem">
+    </div>
     <br>
-    <input type="submit" value="Gravar">
+    <div class="">
+        <label class="block text-sm text-gray-600" for="idade">Idade</label>
+    <input type="number" name="idade" placeholder="Idade" value="{{ old('idade') }}"  class="w-full px-5 py-1 text-gray-700 bg-gray-200 rounded" id="nome" name="nome" type="text" 
+    required="" placeholder="Seu Nome" aria-label="Nome">
+ </div>
+    <br>
+    <input type="submit" value="Gravar" class="px-4 py-1 text-white font-light tracking-wider bg-gray-900 rounded" type="submit">
 </form>
 @endsection
--- resources/views/animais/index.blade.php
+++ resources/views/animais/index.blade.php
@@ -6,22 +6,29 @@
 
 @section('conteudo')
 <p>
-    <a href="{{ route('animais.cadastrar') }}"  class="px-4 py-1 text-white font-light tracking-wider bg-blue-800 rounded"><i class="fas fa-plus mr-3"></i> Cadastrar animal</a>
+    <a  href="{{ route('animais.cadastrar') }}"  class="px-4 py-1 text-white font-light tracking-wider bg-blue-800 rounded"><i class="fas fa-plus mr-3"></i> Cadastrar animal</a>
 </p>
 <p>Veja nossa lista de animais para adoção</p>
 
-<table border="1">
-    <tr>
-        <th>Nome</th>
-        <th>Idade</th>
+<div class="bg-white overflow-auto">
+<table border="1" class="min-w-full w-56 bg-white">  
+    <thead class="bg-gray-800 text-white ">
+    <tr class="bg-gray-800 text-white min-w-full">
+        <th class="w-1/2 text-left py-3 px-4 uppercase font-semibold text-sm">Nome</th>
+        <th class="w-1/2 text-left py-3 px-4 uppercase font-semibold text-sm">Idade</th>
+        <th class="w-1/2 text-left py-3 px-4 uppercase font-semibold text-sm"></th>
     </tr>
-
+</thead>
+</div>
     @foreach ($animais as $animal)
-    <tr>
-        <td>{{ $animal['nome'] }}</td>
-        <td>{{ $animal['idade'] }}</td>
-        <td><a href="{{ route('animais.apagar', $animal['id']) }}">Apagar</a></td>
-    </tr>
+    <tr @if ($loop->even) class="bg-gray-200" @endif>
+        <div>
+        <td class="w-1/2 text-left py-3 px-4">{{ $animal['nome'] }}</td>
+        <td class="w-1/2 text-left py-3 px-4">{{ $animal['idade'] }}</td>
+        <td class=" text-left py-3 px-4 relative  px-3 py-1 font-semibold text-red-900 leading-tight h-9
+         inset-0 bg-red-200 opacity-90 rounded-full"><a href="{{ route('animais.apagar', $animal['id']) }}">Apagar</a></td>
+</div>
+</tr>
     @endforeach
 
 </table>
